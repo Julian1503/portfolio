6d024fdf6fe4081dfa98c5f09c064c26d4b3ab49
Julian Delgado <juliandelgado@alu.frt.edu.ar>
2023-11-25 18:11:20 +1100

    defining structure and default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import {BrowserRouter} from "react-router-dom";
+import {About, Contact, Experience, Hero, Navbar, Tech, Works} from "./components";
+import Projects from "./components/Projects";
+
 const App = () => {
   return (
-      <div>
-        Hello world
-      </div>
+      <BrowserRouter>
+          <div>
+              <div>
+                  <Navbar />
+                  <Hero />
+              </div>
+              <div>
+                  <About />
+              </div>
+              <div>
+                  <Tech />
+              </div>
+              <Projects/>
+              <div>
+                  <div>
+                      <Experience />
+                  </div>
+              </div>
+              <div>
+                  <Contact />
+              </div>
+          </div>
+      </BrowserRouter>
   );
 }
 
