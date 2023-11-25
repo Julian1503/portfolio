a40ad573f23e9cd36418a25c1ae2d05b0c292273
Julian Delgado <juliandelgado@alu.frt.edu.ar>
2023-11-25 22:07:24 +1100

    adding dependencies for TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/tsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
